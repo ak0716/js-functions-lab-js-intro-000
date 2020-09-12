403a902176a0fc0352ac5b0d26407762d8f94e88
Ariel Reuven Kogan <ar.kogan@gmail.com>
2020-09-12 21:55:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // write your code below!
+function happyHolidays() {
+  return `Happy holidays!`
+}
+
+function happyHolidaysTo(name){
+  return `Happy holidays to ${name}`
+}
+
+function happyCustomHolidayTo(holiday, name) {
+  return `Happy ${holiday} to ${name}`
+}
+
+function holidayCountdown(holiday, days) {
+  return `It's ${days} days until ${holiday}!`
+}
